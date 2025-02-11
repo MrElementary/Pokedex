f118a6c88818d18cc53d71b442ddf276bf59d678
Wihan Stoltz <112081989+MrElementary@users.noreply.github.com>
2025-02-11 15:26:01 +0200

    basic REPL created

--- main.go
+++ main.go
@@ -1,9 +1,31 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strings"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	scanner := bufio.NewScanner(os.Stdin)
+	fmt.Println("Type 'exit' to quit")
+
+	for {
+		fmt.Print("Pokedex > ")
+
+		scanner.Scan()
+		input := scanner.Text()
+		input = strings.ToLower(input)
+		input_arr := strings.Fields(input)
+
+		fmt.Printf("Your command was: %v\n", input_arr[0])
+
+		if input_arr[0] == "exit" {
+			break
+		}
+	}
+	if err := scanner.Err(); err != nil {
+		fmt.Fprintln(os.Stdout, "Error:", err)
+	}
 }
