1a316a860d2029ab410db802522126302200a7c6
Wihan Stoltz <112081989+MrElementary@users.noreply.github.com>
2025-02-11 16:51:47 +0200

    added help and exit callbacks

--- main.go
+++ main.go
@@ -1,31 +1,9 @@
 package main
 
 import (
-	"bufio"
-	"fmt"
-	"os"
-	"strings"
+	"github.com/MrElementary/Pokedex/utils"
 )
 
 func main() {
-	scanner := bufio.NewScanner(os.Stdin)
-	fmt.Println("Type 'exit' to quit")
-
-	for {
-		fmt.Print("Pokedex > ")
-
-		scanner.Scan()
-		input := scanner.Text()
-		input = strings.ToLower(input)
-		input_arr := strings.Fields(input)
-
-		fmt.Printf("Your command was: %v\n", input_arr[0])
-
-		if input_arr[0] == "exit" {
-			break
-		}
-	}
-	if err := scanner.Err(); err != nil {
-		fmt.Fprintln(os.Stdout, "Error:", err)
-	}
+	utils.BeginRepl()
 }
--- utils/repl.go
+++ utils/repl.go
@@ -1,6 +1,17 @@
 package utils
 
-import "strings"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
 
 // used to clean strings of clutter
 func CleanInput(text string) []string {
@@ -9,4 +20,63 @@ func CleanInput(text string) []string {
 	clean_text_arr := strings.Fields(clean_text)
 
 	return clean_text_arr
+}
+
+func BeginRepl() {
+
+	scanner := bufio.NewScanner(os.Stdin)
+	fmt.Println("Type 'exit' to quit")
+
+	for {
+		fmt.Print("Pokedex > ")
+
+		scanner.Scan()
+		input := scanner.Text()
+		input_arr := CleanInput(input)
+		command_key := input_arr[0]
+
+		command, ok := getCommands()[command_key]
+		if ok {
+			err := command.callback()
+			if err != nil {
+				fmt.Println(err)
+			}
+			continue
+		} else {
+			fmt.Print("Unknown command")
+			continue
+		}
+	}
+}
+
+func getCommands() map[string]cliCommand {
+	return map[string]cliCommand{
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    exitCallback,
+		},
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    helpCallback,
+		},
+	}
+}
+
+func exitCallback() error {
+	fmt.Println("Closing the Pokedex... Goodbye!")
+	os.Exit(0)
+	return nil
+}
+
+func helpCallback() error {
+	fmt.Println("Welcome to the Pokedex!")
+	fmt.Println()
+	fmt.Println("Valid commands:")
+	for _, cmd := range getCommands() {
+		fmt.Printf("%s: %s\n", cmd.name, cmd.description)
+	}
+	fmt.Println()
+	return nil
 }
